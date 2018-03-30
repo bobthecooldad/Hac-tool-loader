512f5ce6501f8fd2301e31203785f6cd51ecf6ec
bobthecooldad <5620065+bobthecooldad@users.noreply.github.com>
2018-03-30 15:04:06 +0100

    Alpha

--- Loader.py
+++ Loader.py
@@ -3,8 +3,8 @@ import time
 import subprocess
 folder_secure = "F:\Extractor\secure"
 folder_update = "F:\Extractor\update"
-folder_normal = "F:\Extractor\ormal"
-folder_output = "F:\Extractor\Extracted"
+folder_normal = r"F:\Extractor\normal"
+folder_output = "F:/Extractor/Extracted/"
 hactool_location = "F:\Extractor\Hactool\hactool.exe"
 
 
@@ -12,32 +12,25 @@ def file_found(path_input):
     filename = os.listdir(path_input)
     return filename
 
-
-def create_folder(folder_location): #Not making a folder but a file
-    if not os.path.isdir(folder_location):
-        os.mkdir(folder_location)
-
-
-
 def file_remover(unsorted):
     unwanted_script = [s for s in unsorted if not ".cnmt" in s]
     return unwanted_script
 
 
 def hactool_run(file_list, output_folder, hactool, input_folder):
-    loop_counter = 1
+    loop_counter = 0
     loop_counters = 1
-    while len(file_list) >= loop_counter:
-        with open(output_folder, 'w') as extract_folder : # Not opening folder as write so permission denied
-            file_name = file_list[loop_counter]
-            loop_counter = loop_counter + 1
-            cmd = (hactool + " -k keys.dat --plaintext " + input_folder + "/" + file_name + " " + extract_folder + "/")
-            subprocess.call(cmd)
-            time.sleep(20)
-            loop_counters = loop_counters + 1
-
-
-
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+    with open("F:/Extractor/Extracted/", mode='w') as output_path:
+        while len(file_list) >= loop_counter:
+                file_name = file_list[loop_counter]
+                loop_counter = loop_counter + 1
+                #cmd = (hactool + " -k keys.dat --plaintext " + input_folder + "/" + file_name + " " + extract_folder + "/")
+                #subprocess.call(cmd)
+                os.system(hactool + " -k keys.dat --plaintext " + str(input_folder) + "/" + str(file_name) + " " + str(output_path) + "/")
+                time.sleep(20)
+                loop_counters = loop_counters + 1
 
 
 secure_files = file_found(path_input=folder_secure)
@@ -46,7 +39,6 @@ normal_files = file_found(path_input=folder_normal)
 secure_purged_files = file_remover(unsorted=secure_files)
 update_purged_files = file_remover(unsorted=update_files)
 normal_purged_files = file_remover(unsorted=normal_files)
-create_folder = create_folder(folder_location = folder_output)
-hactool_run(file_list=secure_purged_files,output_folder = folder_output,hactool=hactool_location,input_folder=folder_secure)
-hactool_run(file_list=update_purged_files,output_folder = folder_output,hactool=hactool_location,input_folder=folder_update)
-hactool_run(file_list=normal_purged_files,output_folder = folder_output,hactool=hactool_location,input_folder=folder_normal)
+hactool_run(file_list=secure_purged_files, output_folder=folder_output, hactool=hactool_location, input_folder=folder_secure)
+hactool_run(file_list=update_purged_files, output_folder=folder_output, hactool=hactool_location, input_folder=folder_update)
+hactool_run(file_list=normal_purged_files, output_folder=folder_output, hactool=hactool_location, input_folder=folder_normal)
